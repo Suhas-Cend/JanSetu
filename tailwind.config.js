7670c8aef56b31676224619251bbfcc70715ec93
Suhas Kusumba <suhascend@gmail.com>
2026-09-25 16:13:35 +0530

    Add Tailwind CSS configuration with animations

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+import tailwindcssAnimate from "tailwindcss-animate";
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        bebas: ['"Bebas Neue"', 'sans-serif'],
+        lora: ['Lora', 'serif'],
+      }
+    },
+  },
+  plugins: [tailwindcssAnimate],
+}
